b847e2bb3a6c0374131eb6762c1d375402700f7a
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2016-02-13 04:59:29 +0100

    Add comment about GitHub credentials
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- scripts/delRemoteBranches.sh
+++ scripts/delRemoteBranches.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# N.B.
+# You should facilitate futher GitHub asking in the loop
+# by adding git credentials, as explained here: https://git-scm.com/docs/git-credential-store,
+# and thus preventing GitHub to prompt for username/pass every time
+
 REMOTE_BRANCHES=`git branch -r | grep -v -E "master|develop" | sed 's#origin/\(.*\)#\1#'`
 
 for branch in $REMOTE_BRANCHES; do
